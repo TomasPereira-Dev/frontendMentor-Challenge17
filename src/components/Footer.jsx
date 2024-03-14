931ed8b156dd5a74ac3839cc5ddab3f4de6b1cba
Tomas Pereira <tomaspereira2k@gmail.com>
2024-03-14 05:47:11 -0300

    añadido footer para desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="relative flex flex-col gap-8 px-4 py-12 bg-bg1">
             <div className="flex flex-col items-center gap-12 
             before:content-[''] before:absolute before:top-0 before:w-1/4 before:p-[2px] before:bg-cta
-            md:items-start md:before:w-[15%]">
+            md:items-start md:before:w-[15%] lg:flex-row lg: justify-between lg:before:w-[8%]">
                 <img src="/shared/desktop/logo.svg" alt=" " />
                 <nav>
                     <ul className="flex flex-col items-center gap-6 md:flex-row">
@@ -16,11 +16,11 @@ const Footer = () => {
                     </ul>
                 </nav>
             </div>
-            <div className="flex flex-col items-center gap-8 ">
+            <div className="flex flex-col items-center gap-8 lg:flex-row lg:justify-between lg:items-end">
                 <p className="text-white/50 text-center font-semibold max-w-[32ch]
-                md:text-left md:max-w-full">Audiophile is an all in one stop to fulfill your audio needs. We&apos;re a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we&apos;re open 7 days a week.</p>
+                md:text-left md:max-w-full lg:max-w-[57ch]">Audiophile is an all in one stop to fulfill your audio needs. We&apos;re a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we&apos;re open 7 days a week.</p>
                 <p className="text-white/50 font-bold text-center md:hidden">Copyright 2021. All Rights Reserved</p>
-                <div className="flex md:justify-between md:w-full">
+                <div className="flex md:justify-between md:w-full lg:w-auto">
                     <p className="hidden text-white/50 font-bold 
                     md:block lg:hidden">Copyright 2021. All Rights Reserved</p>
                     <div className="flex gap-4">
@@ -29,8 +29,8 @@ const Footer = () => {
                         <img src="/shared/desktop/icon-instagram.svg" alt="instagram's logo" />
                     </div>
                 </div>
-                <p className="hidden text-white/75 font-bold lg:block">Copyright 2021. All Rights Reserved</p>
             </div>
+            <p className="hidden text-white/75 font-bold lg:block">Copyright 2021. All Rights Reserved</p>
         </footer>
     )
 }
