79ec219b26edca7112ecb9f2a1730423d7ece815
Tomas Pereira <tomaspereira2k@gmail.com>
2024-03-14 03:17:27 -0300

    añadida landing page para tablets

--- src/components/Shops.jsx
+++ src/components/Shops.jsx
@@ -2,21 +2,21 @@ import { Link } from "react-router-dom"
 
 const Shops = () => {
     return (
-        <div className="grid grid-cols-1 gap-20 px-4 py-16 bg-white lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-20 px-4 py-16 bg-white md:grid-cols-3 md:gap-2">
             <div className="relative flex flex-col items-center pt-16 pb-2 bg-bg2 rounded-lg">
-                <img className="absolute -top-16 w-1/2 object-contain aspect-square lg:-top-20" src="/shared/desktop/image-category-thumbnail-headphones.png" alt="a pair of headphones" />
-                <p className="font-bold mb-2">HEADPHONES</p>
-                <Link className="flex items-center gap-2 text-text1 font-bold">SHOP <img src="/shared/desktop/icon-arrow-right.svg" alt=" " /></Link>
+                <img className="absolute -top-16 w-1/2 z-10 object-contain aspect-square  md:-top-10 lg:-top-20" src="/shared/desktop/image-category-thumbnail-headphones.png" alt="a pair of headphones" />
+                <p className="relative z-50 font-bold mb-2">HEADPHONES</p>
+                <Link className="relative z-50 flex items-center gap-2 text-text1 font-bold">SHOP <img src="/shared/desktop/icon-arrow-right.svg" alt=" " /></Link>
             </div>
             <div className="relative flex flex-col items-center pt-16 pb-2 bg-bg2 rounded-lg">
-                <img className="absolute -top-16 w-1/2 object-contain aspect-square lg:-top-20" src="/shared/desktop/image-category-thumbnail-speakers.png" alt="a speaker" />
-                <p className="font-bold mb-2">SPEAKERS</p>
-                <Link className="flex items-center gap-2 text-text1 font-bold">SHOP <img src="/shared/desktop/icon-arrow-right.svg" alt=" " /></Link>
+                <img className="absolute -top-16 w-1/2 object-contain aspect-square  md:-top-10 lg:-top-20" src="/shared/desktop/image-category-thumbnail-speakers.png" alt="a speaker" />
+                <p className="relative z-50 font-bold mb-2">SPEAKERS</p>
+                <Link className="relative z-50 flex items-center gap-2 text-text1 font-bold">SHOP <img src="/shared/desktop/icon-arrow-right.svg" alt=" " /></Link>
             </div>
             <div className="relative flex flex-col items-center pt-16 pb-2 bg-bg2 rounded-lg ">
-                <img className="absolute -top-16 w-1/2 object-contain aspect-square lg:-top-20" src="/shared/desktop/image-category-thumbnail-earphones.png" alt="a pair of earphones" />
-                <p className="font-bold mb-2">EARPHONES</p>
-                <Link className="flex items-center gap-2 text-text1 font-bold">SHOP <img src="/shared/desktop/icon-arrow-right.svg" alt=" " /></Link>
+                <img className="absolute -top-16 w-1/2 object-contain aspect-square  md:-top-10 lg:-top-20" src="/shared/desktop/image-category-thumbnail-earphones.png" alt="a pair of earphones" />
+                <p className="relative z-50 font-bold mb-2">EARPHONES</p>
+                <Link className="relative z-50 flex items-center gap-2 text-text1 font-bold">SHOP <img src="/shared/desktop/icon-arrow-right.svg" alt=" " /></Link>
             </div>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,35 +5,51 @@ import AboutSection from "../components/AboutSection.jsx"
 const Home = () => {
     return (
         <main className="flex flex-col gap-16 mb-16">
-           <section className="flex items-center justify-center h-screen bg-mobileHero bg-cover bg-no-repeat md:bg-tabletHero lg:bg-desktopHero">
+           <section className="flex items-center justify-center h-screen bg-mobileHero bg-center bg-cover bg-no-repeat md:bg-tabletHero lg:bg-desktopHero">
                 <div className="flex flex-col items-center justify-center text-center">
-                    <p className="text-white/75 font-thin tracking-[0.6rem] mb-8">NEW PRODUCT</p>
-                    <h1 className="text-4xl text-white font-bold max-w-[10ch]">XX99 MARK II HEADPHONE</h1>
-                    <p className="text-white/75 my-8 max-w-[32ch]">Experience natural, lifelike audio and exeptional build quality made for the passionate music enthusiast.</p>
+                    <p className="text-white/75 font-thin tracking-[0.6rem] mb-8 md:text-sm">NEW PRODUCT</p>
+                    <h1 className="text-4xl text-white font-bold max-w-[10ch] md:text-6xl">XX99 MARK II HEADPHONE</h1>
+                    <p className="text-white/75 my-8 max-w-[32ch] md:max-w-[38ch]">Experience natural, lifelike audio and exeptional build quality made for the passionate music enthusiast.</p>
                     <SeeProductBtn classes={`text-white bg-cta`}/>
                 </div>
            </section>
            <section>
                 <Shops />
            </section>
-           <section className="grid grid-cols-1 gap-4 px-4">
-                <div className="flex flex-col items-center gap-6 px-4 py-8 bg-cta bg-patternCircles bg-no-repeat bg-top bg-contain rounded-lg">
-                    <img className="w-1/2" src="/home/mobile/image-speaker-zx9.png" alt="audiophile's zx9 speaker" />
-                    <h2 className="text-4xl text-white text-center font-bold max-w-[8ch] ">ZX9 SPEAKER</h2>
-                    <p className="text-center text-white/75 max-w-[30ch]">Upgrade to premium speakers that are phenomenally build to deliver truly remarkable sound.</p>
+           <section className="grid grid-cols-1 gap-4 px-4 md:grid-cols-2">
+                <div className="flex flex-col items-center gap-6 px-4 py-8 
+                bg-cta bg-patternCircles bg-no-repeat bg-top bg-contain rounded-lg
+                md:col-span-2">
+                    <picture className="w-1/2 md:w-1/4" >
+                        <source srcSet="/home/desktop/image-speaker-zx9.png" media="(min-width: 1024px)"/>
+                        <source srcSet="/home/tablet/image-speaker-zx9.png" media="(min-width: 768px)"/>
+                        <img src="/home/mobile/image-speaker-zx9.png" alt="audiophile's zx9 speaker" />
+                    </picture>
+                    <h2 className="text-4xl text-white text-center font-bold max-w-[8ch] 
+                    md:text-6xl">ZX9 SPEAKER</h2>
+                    <p className="text-center text-white/75 max-w-[30ch]
+                    md:max-w-[36ch]">Upgrade to premium speakers that are phenomenally build to deliver truly remarkable sound.</p>
                     <SeeProductBtn classes={`text-white bg-black`}/>
                 </div>
-                <div className="relative rounded-lg overflow-hidden">
-                    <img src="/home/mobile/image-speaker-zx7.jpg" alt="audiophile's zx7 speakers" />
+                <div className="relative rounded-lg overflow-hidden md:col-span-2">
+                    <picture>
+                        <source srcSet="/home/desktop/image-speaker-zx7.jpg" media="(min-width: 1024px)"/>
+                        <source srcSet="/home/tablet/image-speaker-zx7.jpg " media="(min-width: 768px)"/>
+                        <img className="w-full" src="/home/mobile/image-speaker-zx7.jpg" alt="audiophile's zx7 speakers" />
+                    </picture>
                     <div className="absolute top-1/4 left-4 flex flex-col gap-8">
                         <h2 className="text-3xl font-bold">ZX7 SPEAKER</h2>
                         <SeeProductBtn classes={`text-black bg-transparent border border-black`}/>
                     </div>
                 </div>
                 <div className="rounded-lg overflow-hidden">
-                    <img src="home/mobile/image-earphones-yx1.jpg" alt="audiophile's yx1 earphones" />
+                    <picture>
+                        <source srcSet="/home/desktop/image-earphones-yx1.jpg" media="(min-width: 1024px)"/>
+                        <source srcSet="/home/tablet/image-earphones-yx1.jpg" media="(min-width: 768px)"/>
+                        <img src="/home/mobile/image-earphones-yx1.jpg" alt="audiophile's yx1 earphones" />
+                    </picture>
                 </div>
-                <div className="flex flex-col gap-8 px-4 py-8 bg-bg2 rounded-lg">
+                <div className="flex flex-col justify-center gap-8 px-4 py-8 bg-bg2 rounded-lg">
                     <h2 className="text-3xl font-bold">YX1 EARPHONES</h2>
                     <SeeProductBtn classes={`text-black bg-transparent border border-black`}/>
                 </div>
