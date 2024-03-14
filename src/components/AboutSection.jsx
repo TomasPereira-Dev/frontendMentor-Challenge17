e28447e7f824c396cce1716c1d749f71176c85ac
Tomas Pereira <tomaspereira2k@gmail.com>
2024-03-14 05:28:06 -0300

    añadido componente about para desktop

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,14 +1,17 @@
 const AboutSection = () => {
     return (
-        <section className="flex flex-col items-center gap-10 px-4">
+        <section className="flex flex-col items-center gap-10 px-4 lg:flex-row-reverse lg:justify-between lg:gap-14">
             <picture>
                 <source srcSet="/shared/desktop/image-best-gear.jpg" media="(min-width: 1024px)"/>
                 <source srcSet="/shared/tablet/image-best-gear.jpg" media="(min-width: 768px)"/>
                 <img className="rounded-lg" src="/shared/mobile/image-best-gear.jpg" alt="A man listening to music" />
             </picture>
-            <h2 className="text-3xl text-center font-bold max-w-[14ch]
-            md:text-5xl md:max-w-[20ch]">BRINGING YOU THE <span className="text-cta">BEST</span> AUDIO GEAR</h2>
-            <p className="text-center text-black/65 font-medium max-w-[32ch] md:max-w-[62ch]">Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
+            <div className="flex flex-col gap-4 items-center text-center lg:items-start lg:text-left">
+                <h2 className="text-3xl  font-bold max-w-[14ch]
+                md:text-5xl md:max-w-[20ch]">BRINGING YOU THE <span className="text-cta">BEST</span> AUDIO GEAR</h2>
+                <p className="text-black/65 font-medium max-w-[32ch] leading-7 md:max-w-[62ch] lg:max-w-[48ch]">Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
+            </div>
+
         </section>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,13 @@ import AboutSection from "../components/AboutSection.jsx"
 const Home = () => {
     return (
         <main className="flex flex-col gap-16 mb-16">
-           <section className="flex items-center justify-center h-screen bg-mobileHero bg-center bg-cover bg-no-repeat md:bg-tabletHero lg:bg-desktopHero">
-                <div className="flex flex-col items-center justify-center text-center">
-                    <p className="text-white/75 font-thin tracking-[0.6rem] mb-8 md:text-sm">NEW PRODUCT</p>
-                    <h1 className="text-4xl text-white font-bold max-w-[10ch] md:text-6xl">XX99 MARK II HEADPHONE</h1>
-                    <p className="text-white/75 my-8 max-w-[32ch] md:max-w-[38ch]">Experience natural, lifelike audio and exeptional build quality made for the passionate music enthusiast.</p>
+           <section className="flex items-center justify-center h-screen px-8 bg-mobileHero bg-center bg-cover bg-no-repeat 
+            md:bg-tabletHero lg:justify-start lg:bg-desktopHero">
+                <div className="flex flex-col items-center gap-4 justify-center text-center
+                lg:items-start lg:gap-6 lg:text-left">
+                    <p className="text-white/75 font-thin tracking-[0.6rem] md:text-sm">NEW PRODUCT</p>
+                    <h1 className="text-4xl text-white font-bold max-w-[10ch] md:text-6xl">XX99 MARK II HEADPHONES</h1>
+                    <p className="text-white/75 max-w-[32ch] leading-7 md:max-w-[38ch]">Experience natural, lifelike audio and exeptional build quality made for the passionate music enthusiast.</p>
                     <SeeProductBtn classes={`text-white bg-cta`}/>
                 </div>
            </section>
@@ -17,19 +19,19 @@ const Home = () => {
                 <Shops />
            </section>
            <section className="grid grid-cols-1 gap-4 px-4 md:grid-cols-2">
-                <div className="flex flex-col items-center gap-6 px-4 py-8 
-                bg-cta bg-patternCircles bg-no-repeat bg-top bg-contain rounded-lg
-                md:col-span-2">
-                    <picture className="w-1/2 md:w-1/4" >
+                <div className="flex flex-col items-center gap-6 px-4 py-8
+                bg-cta bg-patternCircles bg-no-repeat bg-top bg-[length:75vw] rounded-lg
+                md:col-span-2 lg:flex-row lg:justify-center lg:gap-48 lg:py-16 lg:bg-[length:56vw] lg:bg-left">
+                    <picture className="w-1/2 md:w-1/4 lg:relative lg:-bottom-16" >
                         <source srcSet="/home/desktop/image-speaker-zx9.png" media="(min-width: 1024px)"/>
                         <source srcSet="/home/tablet/image-speaker-zx9.png" media="(min-width: 768px)"/>
                         <img src="/home/mobile/image-speaker-zx9.png" alt="audiophile's zx9 speaker" />
                     </picture>
-                    <h2 className="text-4xl text-white text-center font-bold max-w-[8ch] 
-                    md:text-6xl">ZX9 SPEAKER</h2>
-                    <p className="text-center text-white/75 max-w-[30ch]
-                    md:max-w-[36ch]">Upgrade to premium speakers that are phenomenally build to deliver truly remarkable sound.</p>
-                    <SeeProductBtn classes={`text-white bg-black`}/>
+                    <div className="flex flex-col items-center text-center gap-6 lg:items-start lg:text-start">
+                        <h2 className="text-4xl text-white  font-bold max-w-[8ch]md:text-6xl ">ZX9 SPEAKER</h2>
+                        <p className=" text-white/75 max-w-[30ch] leading-7 md:max-w-[36ch]">Upgrade to premium speakers that are phenomenally build to deliver truly remarkable sound.</p>
+                        <SeeProductBtn classes={`text-white bg-black`}/>
+                    </div>
                 </div>
                 <div className="relative rounded-lg overflow-hidden md:col-span-2">
                     <picture>
@@ -37,7 +39,7 @@ const Home = () => {
                         <source srcSet="/home/tablet/image-speaker-zx7.jpg " media="(min-width: 768px)"/>
                         <img className="w-full" src="/home/mobile/image-speaker-zx7.jpg" alt="audiophile's zx7 speakers" />
                     </picture>
-                    <div className="absolute top-1/4 left-4 flex flex-col gap-8">
+                    <div className="absolute top-[35%] left-4 flex flex-col gap-8 lg:left-24">
                         <h2 className="text-3xl font-bold">ZX7 SPEAKER</h2>
                         <SeeProductBtn classes={`text-black bg-transparent border border-black`}/>
                     </div>
@@ -46,10 +48,11 @@ const Home = () => {
                     <picture>
                         <source srcSet="/home/desktop/image-earphones-yx1.jpg" media="(min-width: 1024px)"/>
                         <source srcSet="/home/tablet/image-earphones-yx1.jpg" media="(min-width: 768px)"/>
-                        <img src="/home/mobile/image-earphones-yx1.jpg" alt="audiophile's yx1 earphones" />
+                        <img className="w-full" src="/home/mobile/image-earphones-yx1.jpg" alt="audiophile's yx1 earphones" />
                     </picture>
                 </div>
-                <div className="flex flex-col justify-center gap-8 px-4 py-8 bg-bg2 rounded-lg">
+                <div className="flex flex-col justify-center gap-8 px-4 py-8 bg-bg2 rounded-lg*
+                lg:px-24">
                     <h2 className="text-3xl font-bold">YX1 EARPHONES</h2>
                     <SeeProductBtn classes={`text-black bg-transparent border border-black`}/>
                 </div>
