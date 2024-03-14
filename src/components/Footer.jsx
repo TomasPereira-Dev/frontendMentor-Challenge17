95b793912693f286f043f037d4df3942d5a484eb
Tomas Pereira <tomaspereira2k@gmail.com>
2024-03-14 03:53:15 -0300

    añadido footer y header para tablet

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,11 @@ const Footer = () => {
     return (
         <footer className="relative flex flex-col gap-8 px-4 py-12 bg-bg1">
             <div className="flex flex-col items-center gap-12 
-            before:content-[''] before:absolute before:top-0 before:w-1/4 before:p-[2px] before:bg-cta">
+            before:content-[''] before:absolute before:top-0 before:w-1/4 before:p-[2px] before:bg-cta
+            md:items-start md:before:w-[15%]">
                 <img src="/shared/desktop/logo.svg" alt=" " />
                 <nav>
-                    <ul className="flex flex-col items-center gap-6">
+                    <ul className="flex flex-col items-center gap-6 md:flex-row">
                         <li><NavLink className="text-white font-semibold" to='/'>HOME</NavLink></li>
                         <li><NavLink className="text-white font-semibold" to='/headphones'>HEADPHONES</NavLink></li>
                         <li><NavLink className="text-white font-semibold" to='/speakers'>SPEAKERS</NavLink></li>
@@ -16,12 +17,17 @@ const Footer = () => {
                 </nav>
             </div>
             <div className="flex flex-col items-center gap-8 ">
-                <p className="text-white/75 text-center font-semibold max-w-[32ch]">Audiophile is an all in one stop to fulfill your audio needs. We&apos;re a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we&apos;re open 7 days a week.</p>
-                <p className="text-white/75 font-bold text-center lg:hidden">Copyright 2021. All Rights Reserved</p>
-                <div className="flex gap-2">
-                    <img src="/shared/desktop/icon-facebook.svg" alt="facebook's logo" />
-                    <img src="/shared/desktop/icon-twitter.svg" alt="twitter's logo" />
-                    <img src="/shared/desktop/icon-instagram.svg" alt="instagram's logo" />
+                <p className="text-white/50 text-center font-semibold max-w-[32ch]
+                md:text-left md:max-w-full">Audiophile is an all in one stop to fulfill your audio needs. We&apos;re a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we&apos;re open 7 days a week.</p>
+                <p className="text-white/50 font-bold text-center md:hidden">Copyright 2021. All Rights Reserved</p>
+                <div className="flex md:justify-between md:w-full">
+                    <p className="hidden text-white/50 font-bold 
+                    md:block lg:hidden">Copyright 2021. All Rights Reserved</p>
+                    <div className="flex gap-4">
+                        <img src="/shared/desktop/icon-facebook.svg" alt="facebook's logo" />
+                        <img src="/shared/desktop/icon-twitter.svg" alt="twitter's logo" />
+                        <img src="/shared/desktop/icon-instagram.svg" alt="instagram's logo" />
+                    </div>
                 </div>
                 <p className="hidden text-white/75 font-bold lg:block">Copyright 2021. All Rights Reserved</p>
             </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,19 @@ const Header = () => {
     const [hamburgerIsOpen, setHamburgerIsOpen] = useState(false)
 
     return(
-        <header className="relative flex justify-between items-center p-8 bg-black border-b">
-            <img className="lg:hidden" src="/shared/tablet/icon-hamburger.svg" alt=" "  onClick={() => {setHamburgerIsOpen(!hamburgerIsOpen)}}/>
-            <Link to='/'><img src="/shared/desktop/logo.svg" alt="audiophile's logo" /></Link>
+        <header className="relative flex justify-between items-center w-full p-8 bg-black border-b">
+            <div className="flex justify-between items-center gap-2 "> 
+                <img className="lg:hidden" src="/shared/tablet/icon-hamburger.svg" alt=" "  onClick={() => {setHamburgerIsOpen(!hamburgerIsOpen)}}/>
+                <Link to='/'><img src="/shared/desktop/logo.svg" alt="audiophile's logo" /></Link>
+            </div>
             <nav className="hidden lg:block">
-                <ul className="flex">
-                    <li><NavLink to='/'>HOME</NavLink></li>
-                    <li><NavLink to='/headphones'>HEADPHONES</NavLink></li>
-                    <li><NavLink to='/speakers'>SPEAKERS</NavLink></li>
-                    <li><NavLink to='/earphones'>EARPHONES</NavLink></li>
-                </ul>
-            </nav>
+                    <ul className="flex gap-4 text-white text-sm font-semibold">
+                        <li><NavLink to='/'>HOME</NavLink></li>
+                        <li><NavLink to='/headphones'>HEADPHONES</NavLink></li>
+                        <li><NavLink to='/speakers'>SPEAKERS</NavLink></li>
+                        <li><NavLink to='/earphones'>EARPHONES</NavLink></li>
+                    </ul>
+                </nav>
             <img src="/shared/desktop/icon-cart.svg" alt=" " />
             {hamburgerIsOpen && <HamburgerMenu />}
         </header>
