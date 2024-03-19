e1d3c55d420813ab1374a24257265190f2767963
Tomas Pereira <tomaspereira2k@gmail.com>
2024-03-19 09:33:25 -0300

    añadido pagina de productos para tablet y desktop

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -15,10 +15,10 @@ const ProductPage = () => {
     const productData = data.filter((product) => productName.toLocaleLowerCase() === product.slug.toLocaleLowerCase())
 
     return(
-        <main className="flex flex-col gap-8 p-8">
+        <main className="flex flex-col gap-8 p-8 lg:gap-24">
             <button className="w-fit" onClick={() => navigate(-1)}>Go Back</button>
             <section className="flex flex-col">
-                <div className="flex flex-col gap-4 md:flex-row">
+                <div className="flex flex-col gap-12 md:flex-row">
                     <picture>
                         <source srcSet={`${productData[0].image.desktop}`} media='(min-width: 1024px)'/>
                         <source srcSet={`${productData[0].image.tablet}`} media='(min-width: 768px)'/>
@@ -27,7 +27,7 @@ const ProductPage = () => {
                     <div className="flex flex-col justify-center gap-4">
                         <p className={`${!productData[0].new && 'hidden'} text-cta tracking-[.5rem]`}>NEW PRODUCT</p>
                         <h1 className="text-3xl font-bold max-w-[16ch]">{productData[0].name.toUpperCase()}</h1>
-                        <p className="text-black/50 font-semibold max-w-[35ch]">{productData[0].description}</p>
+                        <p className="text-black/50 font-semibold max-w-[35ch] md:max-w-[48ch] lg:max-w-[78ch]">{productData[0].description}</p>
                         <p className="text-xl font-bold">${productData[0].price}</p>
                         <div className="flex gap-4">
                             <div className="flex items-center justify-center gap-4 px-6 py-3 text-xl bg-bg2">
@@ -38,12 +38,12 @@ const ProductPage = () => {
                     </div>
                 </div>   
             </section>
-            <section className="flex flex-col gap-4 lg:flex-row">
+            <section className="flex flex-col gap-8 lg:flex-row lg:gap-24">
                 <div className="flex flex-col gap-4">
                     <h2 className="text-2xl font-bold">FEATURES</h2>
-                    <p className="text-black/50 font-semibold whitespace-break-spaces">{productData[0].features}</p>
+                    <p className="text-black/50 font-semibold whitespace-break-spaces lg:max-w-[42ch]">{productData[0].features}</p>
                 </div>
-                <div className="flex flex-col gap-4 md:flex-row md:gap-32">
+                <div className="flex flex-col gap-4 md:flex-row md:gap-32 lg:flex-col lg:gap-4">
                     <h2 className="text-2xl font-bold">IN THE BOX</h2>
                     <ul className="flex flex-col gap-2">
                         {Array.from(productData[0].includes).map((items) => <li className="text-black/50" key={items.item}>
@@ -52,7 +52,7 @@ const ProductPage = () => {
                     </ul>
                 </div>
             </section>
-            <section className="grid grid-flow-row gap-4 md:grid-cols-2">
+            <section className="grid grid-flow-row gap-6 md:grid-cols-2">
                 <picture>
                     <source srcSet={productData[0].gallery.first.desktop} media="min-width(1024px)"/>
                     <source srcSet={productData[0].gallery.first.tablet} media="min-width(768px)"/>
@@ -71,12 +71,12 @@ const ProductPage = () => {
             </section>
             <section>
                 <h2 className="text-2xl font-bold mb-4 text-center">YOU MAY ALSO LIKE</h2>
-                <div className="grid grid-cols-1 gap-8">
+                <div className="grid gap-6 md:grid-flow-col">
                     {Array.from(productData[0].others).map((item) => 
                         <div className="flex flex-col items-center gap-4" key={item.name}>
                             <picture>
-                                <source srcSet={item.image.desktop} media="min-width(1024px)"/>
-                                <source srcSet={item.image.tablet} media="min-width(768px)"/>
+                                <source srcSet={item.image.desktop} media="(min-width: 1024px)"/>
+                                <source srcSet={item.image.tablet} media="(min-width: 768px)"/>
                                 <img className="rounded-md" src={item.image.mobile} alt={`audiophile's ${item.name}`} />
                             </picture>
                             <h3 className="text-2xl font-bold">{item.name}</h3>
