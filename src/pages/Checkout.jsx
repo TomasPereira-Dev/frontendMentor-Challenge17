06b91decf5d1cefa8bcacd35877dba2948a85141
tomas pereira <tomaspereira2k@gmail.com>
2024-03-31 16:55:44 -0300

    añadido validacion y mensajes de error al checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -3,33 +3,36 @@ import { useContext } from 'react';
 import { Context } from '../context/context.jsx';
 
 const Checkout = () => {
-    
+    const emailRegex = /[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/i;
     const {productList, finalTotal, setProductList, setFinalTotal} = useContext(Context);
 
     const {handleSubmit, register, formState: { errors }, watch} = useForm();
     const paymentMethod = watch("paymentMethod", "mercadoPago");
-
+    console.log(errors)
     return (
         <main className="p-4 lg:px-8 lg:py-16 bg-bg2 bleeding-3">
             <button className="mb-4 text-semibold text-text1">Go Back</button>
             <section className='grid grid-cols-1 gap-4 lg:grid-cols-3 md:gap-8'>
                 <div className='p-4 bg-white rounded-md md:p-8 lg:col-span-2'>
                     <h1 className="mb-12 text-2xl font-bold lg:text-3xl">CHECKOUT</h1>
-                    <form className='flex flex-col gap-8 text-sm' onSubmit={handleSubmit((data) => console.log(data))}> 
+                    <form className='flex flex-col gap-8 text-sm' id='checkout-form' onSubmit={handleSubmit((data) => console.log(data))}> 
                         <div>
                             <h2 className='mb-4 text-cta font-bold'>BILLING DETAILS</h2>
                             <div className='grid gap-4 md:grid-cols-2'>
                                 <label className='flex flex-col gap-2 font-bold'>
                                     Name
-                                    <input className='p-4 border rounded-md outline-none focus:border-cta' {...register("name", {required: 'this is required'})} type="text" placeholder='Tomas Pereira'/>
+                                    <input className='p-4 border rounded-md outline-none focus:border-cta' {...register("name", {required: 'this is required', pattern: {value: /A-Za-z/, message: `please write your name`}})} type="text" placeholder='Tomas Pereira'/>
+                                    {errors.name && <p className='text-xs text-red-500 font-bold'>{errors.name.message}</p>}
                                 </label>
                                 <label className='flex flex-col gap-2 font-bold'>
                                     Email
-                                    <input className='p-4 border rounded-md outline-none focus:border-cta' {...register("email", {required: 'this is required'})} type="email" placeholder='tomaspereira@example.com'/>
+                                    <input className='p-4 border rounded-md outline-none focus:border-cta' {...register("email", {required: 'this is required', pattern: {value: emailRegex, message: `please write your email`}})} type="email" placeholder='tomaspereira@example.com'/>
+                                    {errors.email && <p className='text-xs text-red-500 font-bold'>{errors.email.message}</p>}
                                 </label>
                                 <label className='flex flex-col gap-2 font-bold'>
                                     Phone Number
-                                    <input className='p-4 border rounded-md outline-none focus:border-cta' {...register("phoneNumber", {required: 'this is required'})} type="number" placeholder='+54 9 11 2122 5152'/>
+                                    <input className='p-4 border rounded-md outline-none focus:border-cta' {...register("phoneNumber", {required: 'this is required', pattern: {value: /A-Za-z/, message: `please write your phone number`}})} type="number" placeholder='+54 9 11 2122 5152'/>
+                                    {errors.phoneNumber && <p className='text-xs text-red-500 font-bold'>{errors.phoneNumber.message}</p>}
                                 </label>
                             </div>
                         </div>
@@ -38,19 +41,25 @@ const Checkout = () => {
                             <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
                                 <label className='flex flex-col gap-2 font-bold md:col-span-2'>
                                     Your Address
-                                    <input className='p-4 border rounded-md outline-none focus:border-cta' {...register("address", {required: 'this is required'})} type="text" placeholder='123 fake street'/>
+                                    <input className='p-4 border rounded-md outline-none focus:border-cta' {...register("address", {required: 'this is required', pattern: {value: /A-Za-z0-9/, message: `please write your home address`}})} type="text" placeholder='123 fake street'/>
+                                    {errors.address && <p className='text-xs text-red-500 font-bold'>{errors.address.message}</p>}
                                 </label>
                                 <label className='flex flex-col gap-2 font-bold'>
                                     ZIP Code   
-                                    <input className='p-4 border rounded-md outline-none focus:border-cta' {...register("zipCode", {required: 'this is required'})} type="number" placeholder='029'/>
+                                    <input className='p-4 border rounded-md outline-none focus:border-cta' {...register("zipCode", {required: 'this is required', 
+                                    minLength: {value: 3, message: 'zip code needs 3 digits'}, maxLength: {value: 3}})}
+                                    type="number" placeholder='029'/>
+                                    {errors.zipCode && <p className='text-xs text-red-500 font-bold'>{errors.zipCode.message}</p>}
                                 </label>
                                 <label className='flex flex-col gap-2 font-bold'>
                                     City
-                                    <input className='p-4 border rounded-md outline-none focus:border-cta' {...register("city", {required: 'this is required'})} type="text" placeholder='Springfield'/>
+                                    <input className='p-4 border rounded-md outline-none focus:border-cta' {...register("city", {required: 'this is required', pattern: {value: /A-Za-z/, message: `please write your city`}})} type="text" placeholder='Springfield'/>
+                                    {errors.city && <p className='text-xs text-red-500 font-bold'>{errors.city.message}</p>}
                                 </label>
                                 <label className='flex flex-col gap-2 font-bold'>
                                     Country
-                                    <input className='p-4 border rounded-md outline-none focus:border-cta' {...register("country", {required: 'this is required'})} type="text" placeholder='United States'/>
+                                    <input className='p-4 border rounded-md outline-none focus:border-cta' {...register("country", {required: 'this is required', pattern: {value: /A-Za-z/, message: `please write your country`}})} type="text" placeholder='United States'/>
+                                    {errors.country && <p className='text-xs text-red-500 font-bold'>{errors.country.message}</p>}
                                 </label>
                             </div>
                         </div>
@@ -63,7 +72,7 @@ const Checkout = () => {
                                 <div className='p-4 border rounded-md group'>
                                     <label className='flex flex-row-reverse justify-end items-center gap-2'>
                                         MercadoPago
-                                        <input className='p-4 border rounded-md outline-none' {...register("paymentMethod", {required: 'this is required'})} type="radio" value="mercadoPago"/>
+                                        <input className='p-4 border rounded-md outline-none' {...register("paymentMethod", {required: 'this is required'})} type="radio" value="mercadoPago" defaultChecked/>
                                     </label>
                                 </div>
                                 <div className='p-4 border rounded-md peer'>
@@ -72,6 +81,7 @@ const Checkout = () => {
                                         <input className='p-4 border rounded-md outline-none' {...register("paymentMethod", {required: 'this is required'}) } type="radio" value="cashOnDelivery"/>
                                     </label>
                                 </div>
+                                {errors.paymentMethod && <p className='text-xs text-red-500 font-bold'>{errors.paymentMethod.message}</p>}
                             </div>
                             <div className={`${paymentMethod == "mercadoPago" && 'hidden'}`}>
                                 <img className='mb-4' src="/checkout/icon-cash-on-delivery.svg" alt=" " />
@@ -105,11 +115,12 @@ const Checkout = () => {
                         <li className='flex justify-between items-center'><p className='text-text1'>VAT (INCLUDED)</p> <span className='text-lg font-bold'>${Math.round((finalTotal * 20) / 100)}</span></li>
                         <li className='flex justify-between items-center my-8'><p className='text-text1'>GRAND TOTAL</p> <span className='text-lg text-cta font-bold'>${Math.round(finalTotal + 50 + (finalTotal * 20) / 100)}</span></li>
                     </ul>
-                    <button className='py-2 text-white font-bold bg-cta'>CONTINUE & PAY</button>
+                    <button className='py-2 text-white font-bold bg-cta' type='submit' form='checkout-form'>CONTINUE & PAY</button>
                 </div>
             </section>
         </main>
     )
+    
 }
 
 export default Checkout
