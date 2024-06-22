53ed2c8ff700702e8395c55b8df8af1d41cfc543
tomas pereira <tomaspereira2k@gmail.com>
2024-06-22 10:33:53 -0300

    typo corregida

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import { createPreferenceRouter } from "./routes/createPreferenceRouter.js";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 0;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
