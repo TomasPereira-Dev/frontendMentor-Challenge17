a9bb748fdc94eea319d7c01c8a06a207dbcfa1d4
tomas pereira <tomaspereira2k@gmail.com>
2024-06-22 10:12:28 -0300

    añadido json de productos

--- server/routes/catalogRouter.js
+++ server/routes/catalogRouter.js
@@ -1,18 +1,10 @@
 import { Router } from "express";
-import { db } from "../index.js";
+import data from "../data.json";
 
 const catalogRouter = Router();
 
-catalogRouter.get("/", async (req, res) => {
-    try{
-        const coll =  db.collection("catalog");
-        const response = await coll.find({}).toArray((err, docs) => {
-            console.log(docs, err)
-        });
-        res.send(response)
-    } catch (error){
-        console.log(error)
-    }
+catalogRouter.get("/", (req, res) => {
+    res.json(data)
 });
 
 export { catalogRouter }
